625b56dd7165bce527fb3f1ff3ee11e4ed86d7fc
Abhimanyu121 <abhimanyushekhawat17.as@gmail.com>
2019-06-16 15:27:49 +0530

    models and al the anomalies fiexed

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config(object):
     TESTING = True
     CSRF_ENABLED = True
     SECRET_KEY = 'this-really-needs-to-be-changed'
-    SQLALCHEMY_DATABASE_URI = "postgresql://localhost/mentors"
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
 
 
 class ProductionConfig(Config):
--- models.py
+++ models.py
@@ -1,4 +1,6 @@
 from app import db
+from sqlalchemy import Table, Column, Integer, ForeignKey
+from sqlalchemy.orm import relationship
 
 class Credentials(db.Model):
 	__tablename__ = 'credentials'
@@ -28,28 +30,32 @@ class Topics(db.Model):
 
 class Mentor_list(db.Model):
 	__tablename__ = 'mentor_list'
+	id = db.Column(db.Integer, primary_key = True)
 	name = db.Column(db.String(), ForeignKey('topics.name'))
 	email = db.Column(db.String(), ForeignKey('profile.email'))
-	profile = relationship("Profile", backref=backref("profile", uselist=False))
-	topics = relationship("Topics", backref=backref("topics", uselist=False))
+	profile = relationship("Profile", backref=("profile"))
+	topics = relationship("Topics", backref=("topics"))
 
 class Timeline(db.Model):
 	__tablename__ = 'timeline'
+	id = db.Column(db.Integer, primary_key = True)
 	name = db.Column(db.String(), ForeignKey('topics.name'))
 	day = db.Column(db.String())
 	goal = db.Column(db.String())
 
 class Enrollment(db.Model):
 	__tablename__ = 'enrollment'
+	id = db.Column(db.Integer, primary_key = True)
 	topic_name = db.Column(db.String(), ForeignKey('topics.name'))
 	mentor = db.Column(db.String(), ForeignKey('profile.email'))
 	mentee = email = db.Column(db.String(), ForeignKey('profile.email'))
-	profile = relationship("Profile", backref=backref("profile", uselist=False))
-	topics = relationship("Topics", backref=backref("topics", uselist=False))
+	profile = relationship("Profile", backref=("profile"))
+	topics = relationship("Topics", backref=("topics"))
 
 class Notification(db.Model):
 	__tablename__ = 'notification'
+	id = db.Column(db.Integer, primary_key = True)
 	mentee = db.Column(db.String(), ForeignKey('profile.email'))
 	mentor = db.Column(db.String(), ForeignKey('profile.email'))
 	request = db.Column(db.Boolean)
-	number = db.Column(db.Integer, Sequence(increment=1),primary_key=True)
+	number = db.Column(db.Integer,primary_key=True)
