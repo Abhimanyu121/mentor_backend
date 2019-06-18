2f6cc3ba8835724279dd28ec0e165818834de5b5
Abhimanyu121 <abhimanyushekhawat17.as@gmail.com>
2019-06-18 10:38:24 +0530

    before nuking the foreign keys

--- app.py
+++ app.py
@@ -14,8 +14,8 @@ from models import *
 @app.route("/")
 def ping():
 	return "ping resp"
-
-@app.route("/register")
+#register and login
+@app.route("/register",methods=['POST'])
 def register():
 	email = request.args.get('email')
 	password = request.args.get('password')
@@ -44,24 +44,127 @@ def register():
 		return"ok"
 		
 	except Exception as e:
-		print(str(e))
+		return str(e)
 
 @app.route("/login")
 def login():
 	email = request.args.get('email')
 	password = request.args.get('password')
 	try:
-		credentials=Credentials.query.filter_by(id=id_).first()
+		credentials=Credentials.query.filter_by(email = email).first()
 		if password == credentials['password']:
 			return True
-		else
+		else:
 			return False
 	except Exception as e:
 		print(str(e))
 		return False
+#setter functions
 
+@app.route("/enroll")
+def enroll():
+	email = request.args.get('email')
+	password = request.args.get('password')
+	try:
+		credentials=Credentials.query.filter_by(email = email).first()
+		if password == credentials['password']:
+			enrollment = Enrollment(
+					mentee = email,
+					mentor = request.args.get('mentor'),
+					status = False,
+					topic_name = request.args.get('topic_name')
+				)
+			notif = Notification(
+					mentee = email,
+					mentor = request.args.get('mentor'),
+					status = False,
+					topic_name = request.args.get('topic_name')
+				)
+			db.session.add(enrollment)
+			db.session.commit()
+			db.session.add(notif)
+			db.session.commit()
+			return True
+		else:
+			return False
+	except Exception as e:
+		print(str(e))
+		return False
+@app.route("/add_timeline")
+def add_timeline():
+	email = request.args.get('email')
+	password = request.args.get('password')
+	try:
+		credentials=Credentials.query.filter_by(email = email).first()
+		if password == credentials['password']:
+			timeline = Timeline(
+					name = request.args.get('name'),
+					day = request.args.get('day'),
+					goal = request.args.get('goal')
+				)
+			db.session.add(timeline)
+			db.session.commit()
+			return True
+		else:
+			return False
+	except Exception as e:
+		print(str(e))
+		return False
+@app.route("/add_mentor")
+def add_mentor():
+	email = request.args.get('email')
+	password = request.args.get('password')
+	try:
+		credentials=Credentials.query.filter_by(email = email).first()
+		if password == credentials['password']:
+			mentor = Mentor(
+					name = request.args.get('name'),
+					email = request.args.get('email')
+				)
+			db.session.add(mentor)
+			db.session.commit()
+			return True
+		else:
+			return False
+	except Exception as e:
+		print(str(e))
+		return False
+@app.route("/add_topics")
+def new_topic():
+	email = request.args.get('email')
+	password = request.args.get('password')
+	try:
+		credentials=Credentials.query.filter_by(email = email).first()
+		if password == credentials['password']:
+			topic = Topic(
+					name = request.args.get('name')
+				)
+			db.session.add(mentor)
+			db.session.commit()
+			return True
+		else:
+			return False
+	except Exception as e:
+		print(str(e))
+		return False
+#getter functions
 
+@app.route("/profile")
+def profile():
+	email = request.args.get('email')
+	password = request.args.get('password')
+	try:
+		credentials=Credentials.query.filter_by(email = email).first()
+		if password == credentials['password']:
+			profile = Profile.query.filter_by(email = email).first()
+			return jsonify(profile.serialize())
+			print (str(profile))
+		else:
+			return None
+	except Exception as e:
+		print(str(e))
+		return None
 migrate = Migrate(app, db)
 if __name__ == '__main__':
 	app.run()
-	
+#localhost:5000/register?email=root&password=root&name=qwerty&location='qwerty'&gender=male&interest=qwerty&college=qwerty
--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 from app import db
-from sqlalchemy import Table, Column, Integer, ForeignKey
+from sqlalchemy import Table, Column, Integer, ForeignKeyConstraint
 from sqlalchemy.orm import relationship
 
 class Credentials(db.Model):
@@ -19,6 +19,18 @@ class Profile(db.Model):
 	interest = db.Column(db.String())
 	gender = db.Column(db.String())
 	location = db.Column(db.String())
+	name = db.Column(db.String())
+	mentors = relationship("Mentor_list", backref="profile", lazy=True)
+	enroll = relationship("Enrollment", backref="profile", lazy=True)
+	notifs = relationship("Notification", backref="profile", lazy=True)
+	def __init__(self, email,password):
+		self.email= email
+		self.college = college
+		self.interest = interest
+		self.gender = gender
+		self.location = location
+		self.name = name
+
 	def __repr__(self):
 		return '<Email %r>' % self.email
 
@@ -26,36 +38,41 @@ class Profile(db.Model):
 
 class Topics(db.Model):
 	__tablename__ = 'topics'
-	name = db.Column(db.String(), primary_key = True)
+	topic_name = db.Column(db.String(), primary_key = True)
+	mentors = relationship("Mentor_list", backref="profile", lazy=True)
+	time = relationship("Timeline", backref="topics", lazy=True)
+	enroll = relationship("Enrollment", backref="topics", lazy=True)
+	notifs = relationship("Notification", backref="topics", lazy=True)
 
 class Mentor_list(db.Model):
 	__tablename__ = 'mentor_list'
 	id = db.Column(db.Integer, primary_key = True)
-	name = db.Column(db.String(), ForeignKey('topics.name'))
-	email = db.Column(db.String(), ForeignKey('profile.email'))
-	profile = relationship("Profile", backref=("profile"))
-	topics = relationship("Topics", backref=("topics"))
+	topic_name = db.Column(db.String())
+	email = db.Column(db.String())
+	ForeignKeyConstraint(['email', 'topic_name'], ['profile.email', 'topics.topic_name'])
 
 class Timeline(db.Model):
 	__tablename__ = 'timeline'
 	id = db.Column(db.Integer, primary_key = True)
-	name = db.Column(db.String(), ForeignKey('topics.name'))
+	topic_name = db.Column(db.String())
 	day = db.Column(db.String())
 	goal = db.Column(db.String())
+	ForeignKeyConstraint(['name'], ['topics.topic_name'])
 
 class Enrollment(db.Model):
 	__tablename__ = 'enrollment'
 	id = db.Column(db.Integer, primary_key = True)
-	topic_name = db.Column(db.String(), ForeignKey('topics.name'))
-	mentor = db.Column(db.String(), ForeignKey('profile.email'))
-	mentee = email = db.Column(db.String(), ForeignKey('profile.email'))
-	profile = relationship("Profile", backref=("profile"))
-	topics = relationship("Topics", backref=("topics"))
-
+	topic_name = db.Column(db.String())
+	mentor = db.Column(db.String())
+	mentee = db.Column(db.String())
+	status = db.Column(db.Boolean)
+	ForeignKeyConstraint(['mentor', 'mentee', ' topic_name'], ['profile.email', 'profile.email','topics.topic_name'])
 class Notification(db.Model):
 	__tablename__ = 'notification'
 	id = db.Column(db.Integer, primary_key = True)
-	mentee = db.Column(db.String(), ForeignKey('profile.email'))
-	mentor = db.Column(db.String(), ForeignKey('profile.email'))
+	topic_name = db.Column(db.String())
+	mentee = db.Column(db.String())
+	mentor = db.Column(db.String())
 	request = db.Column(db.Boolean)
 	number = db.Column(db.Integer,primary_key=True)
+	ForeignKeyConstraint(['mentor', 'mentee', ' topic_name'], ['profile.email', 'profile.email','topics.topic_name'])
